4c5b4f590e9132b1777dd167b4aa2efc49c2caa1
Daniel Watson <watsondaniel6@gmail.com>
2014-08-31 18:43:43 -0500

    Added heroku start

---
+++ bin/start.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+var app = require('../config/app');
+
+var port = process.env.PORT || 3000;
+app.set('port', port);
+
+app.listen(port);
+console.log("Listening on port %d", port);
