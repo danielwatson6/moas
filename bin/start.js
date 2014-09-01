53fbedf5220495fa323332e46ea4b72bfcf5e7c3
Daniel Watson <watsondaniel6@gmail.com>
2014-08-31 19:14:19 -0500

    Heroku 6

--- bin/start.js
+++ bin/start.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var app = require('/javascripts/config/app');
+var app = require('../public/javascripts/config/app');
 
 var port = process.env.PORT || 3000;
 app.set('port', port);
