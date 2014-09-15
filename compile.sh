b380829c9e9328047de40dc5947ea5d6ca2f29e2
Daniel Watson <watsondaniel6@gmail.com>
2014-09-14 20:57:16 -0500

    Corrected script

--- compile.sh
+++ compile.sh
@@ -4,10 +4,10 @@ cp package.json js
 cp .gitignore js
 # app directory
 coffee -bco js/app app
-cp -r app/views js/app/views
+cp -r app/views/* js/app/views
 # assets and public directory
-cp -r assets/ js/assets
-cp -r public/ js/public
+cp -r assets/* js/assets
+cp -r public/* js/public
 # bin, config, and lib directories
 coffee -bco js/bin bin
 coffee -bco js/config config
